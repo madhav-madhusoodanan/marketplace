17fb4d0360edad578eb4393ac66f724fe257ed83
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-04-21 21:11:25 +0400

    more documentation

--- models/user.js
+++ models/user.js
@@ -20,8 +20,8 @@ const Users = new mongoose.Schema({
   activity: {
     type: Number,
     default: 0,
-    // tens digit: safe to bid?
-    // one's digit: safe to post objects?
+    // tens digit: indicates safe to bid?
+    // one's digit: indicates safe to post objects?
     // 1 -> not safe
     // 0 -> safe
     // possible options: 00, 01, 10, 11
